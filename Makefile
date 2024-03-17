d9e6b13c07134336ad174a3af6886718e4f3335a
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-03-17 18:21:20 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+all:  program.s program.o
+	as --32 -o program.o program.s 
+	ld -m elf_i386 -o program program.o
+build:	program.s
+	as --32 -o program.o program.s 
+link: program.o
+	ld -m elf_i386 -o program program.o
+run: program 
+	./wy
+clean:
+	rm -f *.o  program
---
+++ program.s
@@ -0,0 +1,59 @@
+SYS_EXIT = 1
+SYS_READ = 3
+SYS_WRITE = 4
+STDOUT = 1
+STDIN = 0
+SYSCALL = 0x80
+
+.bss 
+    character: .byte 0 
+.text 
+.global _start
+_start:
+    mov $SYS_READ, %eax
+    mov $STDIN, %ebx
+    mov $character, %ecx
+    mov $1, %edx
+    int $SYSCALL
+
+    cmp  $0, %eax
+    je end
+
+    cmpb $65, character 
+    jb wrongCharacter
+
+    cmpb $90, character
+    jbe bigCharacter 
+
+    cmpb $122, character 
+    jbe smallCharacter 
+
+    cmpb $122, character 
+    ja wrongCharacter
+
+wrongCharacter: 
+    movb $'., character
+    jmp display
+
+bigCharacter:
+    addb $0x20, character
+    jmp display
+
+smallCharacter:
+    subb $0x20, character
+    jmp display
+    
+
+
+display:
+    mov $SYS_WRITE, %eax
+    mov $STDOUT, %ebx
+    mov $character, %ecx
+    mov $1, %edx
+    int $SYSCALL
+    jmp _start
+
+end:      
+    mov $SYS_EXIT, %eax
+    int $SYSCALL
+
